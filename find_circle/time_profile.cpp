ea9078076e7fdc0ee8946a549fac31d6db1672c1
darren <darrendarren1231@gmail.com>
2022-06-28 11:47:53 +0800

    modify to array of mat

--- find_circle/time_profile.cpp
+++ find_circle/time_profile.cpp
@@ -16,10 +16,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	Mat img5 = imread("0um/img1-5.bmp", 2);*/
 
 	string filename;
-	Mat source_array[5];
+	Mat source_map[5];
+	Mat binary_map[5];
 	for (int index = 0; index < 5; index++){
 		filename = "img2-" + to_string(index+1) + ".bmp";
-		source_array[index] = imread(filename,2);
+		source_map[index] = imread(filename, 2);
+		binary_map[index] = source_map[index].clone();
 	}
 
 	vector <Mat> source_mat = {};
@@ -36,11 +38,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		binary_mat.push_back(binary_img);
 	}*/
 
+	
 	Mat img1 = imread("img2-1.bmp", 2);
 	Mat img2 = imread("img2-2.bmp", 2);
 	Mat img3 = imread("img2-3.bmp", 2);
 	Mat img4 = imread("img2-4.bmp", 2);
-	Mat img5 = imread("img2-5.bmp", 2);
+	Mat img5 = imread("img2-5.bmp", 2);	
 
 	Mat binary0 = img1.clone();
 	Mat binary1 = img2.clone();
@@ -54,12 +57,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	double threshod;
 	double set_thresshold = 0.65;
 
-	for (int x = 0; x < source_array[0].cols; x++){
-		for (int y = 0; y < 100; y++){
+	for (int x = 0; x < source_map[0].cols; x++){
+		for (int y = 0; y < source_map[0].rows; y++){
 
 			vector<int> v = {};
 			for (int index = 0; index < 5; index++){
-				gray = (int)(source_array[index].at<uchar>(y, x));
+				gray = (int)(source_map[index].at<uchar>(y, x));
 				v.push_back(gray);
 				/*v.push_back((int)(source_mat[index].at<uchar>(y, x)));*/
 
@@ -73,56 +76,68 @@ int _tmain(int argc, _TCHAR* argv[])
 			maxValue = *max_element(v.begin(), v.end());
 			minValue = *min_element(v.begin(), v.end());
 
+
+			for (int index = 0; index < 5; index++){
+				if (maxValue - minValue != 0)
+					threshod = (double)(v[index] - minValue) / (maxValue - minValue);
+				else
+					threshod = 0;
+
+				if (threshod<set_thresshold)
+					binary_map[index].at<uchar>(y, x) = 0;
+				else
+					binary_map[index].at<uchar>(y, x) = 255;
+			}
 			///////////////////////////////////////0
-			if (maxValue - minValue != 0)
-				threshod = (double)(v[0] - minValue) / (maxValue - minValue);
-			else
-				threshod = 0;
-			
-			if (threshod<set_thresshold)
-				binary0.at<uchar>(y, x) = 0;
-			else
-				binary0.at<uchar>(y, x) = 255;
-			////////////////////////////////////////1
-			if (maxValue - minValue != 0)
-				threshod = (double)(v[1] - minValue) / (maxValue - minValue);
-			else
-				threshod = 0;
-			
-			if (threshod<set_thresshold)
-				binary1.at<uchar>(y, x) = 0;
-			else
-				binary1.at<uchar>(y, x) = 255;
-			////////////////////////////////////////2
-			if (maxValue - minValue != 0)
-				threshod = (double)(v[2] - minValue) / (maxValue - minValue);
-			else
-				threshod = 0;
+			//if (maxValue - minValue != 0)
+			//	threshod = (double)(v[0] - minValue) / (maxValue - minValue);
+			//else
+			//	threshod = 0;
+			//
+			//if (threshod<set_thresshold)
+			//	binary0.at<uchar>(y, x) = 0;
+			//else
+			//	binary0.at<uchar>(y, x) = 255;
+			//////////////////////////////////////////1
+			//if (maxValue - minValue != 0)
+			//	threshod = (double)(v[1] - minValue) / (maxValue - minValue);
+			//else
+			//	threshod = 0;
+			//
+			//if (threshod<set_thresshold)
+			//	binary1.at<uchar>(y, x) = 0;
+			//else
+			//	binary1.at<uchar>(y, x) = 255;
+			//////////////////////////////////////////2
+			//if (maxValue - minValue != 0)
+			//	threshod = (double)(v[2] - minValue) / (maxValue - minValue);
+			//else
+			//	threshod = 0;
 
-			if (threshod<set_thresshold)
-				binary2.at<uchar>(y, x) = 0;
-			else
-				binary2.at<uchar>(y, x) = 255;
-			////////////////////////////////////////3
-			if (maxValue - minValue != 0)
-				threshod = (double)(v[3] - minValue) / (maxValue - minValue);
-			else
-				threshod = 0;
+			//if (threshod<set_thresshold)
+			//	binary2.at<uchar>(y, x) = 0;
+			//else
+			//	binary2.at<uchar>(y, x) = 255;
+			//////////////////////////////////////////3
+			//if (maxValue - minValue != 0)
+			//	threshod = (double)(v[3] - minValue) / (maxValue - minValue);
+			//else
+			//	threshod = 0;
 
-			if (threshod<set_thresshold)
-				binary3.at<uchar>(y, x) = 0;
-			else
-				binary3.at<uchar>(y, x) = 255;
-			////////////////////////////////////////4
-			if (maxValue - minValue != 0)
-				threshod = (double)(v[4] - minValue) / (maxValue - minValue);
-			else
-				threshod = 0;
+			//if (threshod<set_thresshold)
+			//	binary3.at<uchar>(y, x) = 0;
+			//else
+			//	binary3.at<uchar>(y, x) = 255;
+			//////////////////////////////////////////4
+			//if (maxValue - minValue != 0)
+			//	threshod = (double)(v[4] - minValue) / (maxValue - minValue);
+			//else
+			//	threshod = 0;
 
-			if (threshod<set_thresshold)
-				binary4.at<uchar>(y, x) = 0;
-			else
-				binary4.at<uchar>(y, x) = 255;
+			//if (threshod<set_thresshold)
+			//	binary4.at<uchar>(y, x) = 0;
+			//else
+			//	binary4.at<uchar>(y, x) = 255;
 
 			/*if (maxValue - minValue != 0)
 				threshod = (double)(v[1] - minValue) / (maxValue - minValue);
@@ -145,15 +160,15 @@ int _tmain(int argc, _TCHAR* argv[])
 				cout << x << " " << y <<" "<<threshod<< endl;*/
 		}
 	}
-	/*for (int index = 0; index < 5; index++)
-		imwrite("binary_ntu_" + to_string(index) + ".jpg", binary_mat[index]);*/
+	for (int index = 0; index < 5; index++)
+		imwrite("binary_ntu_" + to_string(index) + ".jpg", binary_map[index]);
 	
 	
-	imwrite("binary_ntu_" + formatDobleValue(set_thresshold,2) + "_" + to_string(0) + ".jpg", binary0);	
+	/*imwrite("binary_ntu_" + formatDobleValue(set_thresshold,2) + "_" + to_string(0) + ".jpg", binary0);	
 	imwrite("binary_ntu_" + formatDobleValue(set_thresshold, 2) + "_" + to_string(1) + ".jpg", binary1);
 	imwrite("binary_ntu_" + formatDobleValue(set_thresshold, 2) + "_" + to_string(2) + ".jpg", binary2);
 	imwrite("binary_ntu_" + formatDobleValue(set_thresshold, 2) + "_" + to_string(3) + ".jpg", binary3);
-	imwrite("binary_ntu_" + formatDobleValue(set_thresshold, 2) + "_" + to_string(4) + ".jpg", binary4);
+	imwrite("binary_ntu_" + formatDobleValue(set_thresshold, 2) + "_" + to_string(4) + ".jpg", binary4);*/
 
 	///*double scale = 0.25;
 	//resize(binary1, binary1, Size(img1.cols*scale, img1.rows*scale));WW
@@ -162,7 +177,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	namedWindow("HelloCV", WINDOW_AUTOSIZE);
 	imshow("HelloCV", img1);
-	imshow("binary0", binary0);
+	imshow("binary0", binary_map[0]);
 	//
 	waitKey(0);
 	destroyWindow("HelloCV");
